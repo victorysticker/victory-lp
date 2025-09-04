3efb74aa3ed44c7415c9534d20ad12cea0b5ea78
Eriec Tanijaya <eriectan88@gmail.com>
2025-09-04 21:05:59 +0700

    feat: setup tls

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,11 @@ const server = serve({
     "/": landingPage
   },
   hostname: "0.0.0.0",
-  port: 443
+  port: 443,
+  tls: {
+    cert: Bun.file("../../cert.pm"),
+    key: Bun.file("../../key.pm")
+  }
 })
 
 console.log(`Listening on ${server.url}`)
