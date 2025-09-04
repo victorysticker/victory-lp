cc2ddb7e874fa34c911f81aa71916e10bb237ca8
Eriec Tanijaya <eriectan88@gmail.com>
2025-09-04 20:49:51 +0700

    feat: use bun webserver

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { serve } from "bun";
+import landingPage from './index.html';
+
+const server = serve({
+  routes: {
+    "/": landingPage
+  }
+})
+
+console.log(`Listening on ${server.url}`)
