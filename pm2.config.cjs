d9e2567cfe260e81d101e4989b905971f5437d66
Eriec Tanijaya <eriectan88@gmail.com>
2025-09-10 12:07:06 +0700

    fix: issue with pm2 package, where it can't handle top level await
    ref: https://github.com/oven-sh/bun/issues/19942#issuecomment-2974700287

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
 	name: "Victory LP",
-	script: "./src/index.ts",
-	interpreter: "bun",
+	script: "bun",
+	args: "./src/index.ts",
+	interpreter: "none",
 	env: {
 		PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`, // Add "~/.bun/bin/bun" to PATH
 	},
